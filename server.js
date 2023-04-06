8e56a21398988d8cb5f21a39b7a5e9b28316c35a
Jefferson Ventura <jeffersonsilvamuz@gmail.com>
2023-04-05 22:55:33 -0300

    feat: Password hashing usinbg bcrypt and refactoring server.js

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-import express from 'express'
-import mysql from 'mysql'
-import cors from 'cors'
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
-import cookieParser from 'cookie-parser'
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
 
+const saltRounds = 10;
 const app = express();
-app.use(express.json);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-const db = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        password: "12345",
-        database: "carefy"
-    }
-)
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345',
+  database: 'carefy',
+});
 
-app.post('/register', (req, res) => {
-    const sql = "INSERT INTO users ('name', 'email', 'password') VALUES(?)";
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password
-    ]
-})
+app.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password ?? '', saltRounds);
+    const sql = `INSERT INTO users (\`name\`, \`email\`, \`password\`) VALUES (?, ?, ?)`;
+    await db.query(sql, [name, email, hashedPassword]);
+    res.json({ status: 'Sucesso' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
 
 app.listen(8081, () => {
-    console.log("Rodando...");
-})
+  console.log('Servidor rodando na porta 8081...');
+});
